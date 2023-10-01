8098dd31f084a174742fb26e4fe7440abefc03ed
ajh123 <ajh123@users.noreply.github.com>
2023-10-01 13:39:36 +0100

    Fix peachjam settings

--- samlandlii/settings.py
+++ samlandlii/settings.py
@@ -1,21 +1,11 @@
 from django.utils.translation import gettext_lazy as _
 
 from liiweb.settings import *  # noqa
+from peachjam.settings import *  # noqa
 
 INSTALLED_APPS = ["samlandlii.apps.SamlandLIIConfig"] + INSTALLED_APPS  # noqa
 
-PEACHJAM = {
-    "APP_NAME": "SamlandLII",
-    "SUPPORT_EMAIL": os.environ.get("SUPPORT_EMAIL"),
-    "SENTRY_DSN_KEY": os.environ.get("SENTRY_DSN_KEY"),
-    "SENTRY_ENVIRONMENT": os.environ.get("SENTRY_ENVIRONMENT", "staging"),
-    "CITATOR_API": os.environ.get(
-        "CITATOR_API", "https://services.lawsafrica.com/citator/v1/extract-citations"
-    ),
-    "CITATOR_API_KEY": os.environ.get("CITATOR_API_KEY"),
-    "EXTRA_SEARCH_INDEXES": [],
-    "SEARCH_JURISDICTION_FILTER": False,
-}
+PEACHJAM["APP_NAME"] = "SamlandLII"
 
 JAZZMIN_SETTINGS["site_title"] = "SamlandLII"  # noqa
 JAZZMIN_SETTINGS["site_header"] = "SamlandLII"  # noqa
