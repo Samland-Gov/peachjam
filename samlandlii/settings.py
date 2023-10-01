74cf909d9eb88532a3b7aecf2d34522fb4072ba4
ajh123 <ajh123@users.noreply.github.com>
2023-10-01 13:38:38 +0100

    Import other peachjam settings

--- samlandlii/settings.py
+++ samlandlii/settings.py
@@ -5,7 +5,16 @@ from liiweb.settings import *  # noqa
 INSTALLED_APPS = ["samlandlii.apps.SamlandLIIConfig"] + INSTALLED_APPS  # noqa
 
 PEACHJAM = {
-    "APP_NAME": "SamlandLII"
+    "APP_NAME": "SamlandLII",
+    "SUPPORT_EMAIL": os.environ.get("SUPPORT_EMAIL"),
+    "SENTRY_DSN_KEY": os.environ.get("SENTRY_DSN_KEY"),
+    "SENTRY_ENVIRONMENT": os.environ.get("SENTRY_ENVIRONMENT", "staging"),
+    "CITATOR_API": os.environ.get(
+        "CITATOR_API", "https://services.lawsafrica.com/citator/v1/extract-citations"
+    ),
+    "CITATOR_API_KEY": os.environ.get("CITATOR_API_KEY"),
+    "EXTRA_SEARCH_INDEXES": [],
+    "SEARCH_JURISDICTION_FILTER": False,
 }
 
 JAZZMIN_SETTINGS["site_title"] = "SamlandLII"  # noqa
