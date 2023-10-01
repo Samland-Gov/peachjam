40f102c4787f580f6d26de0bc209d5db8840b562
ajh123 <ajh123@users.noreply.github.com>
2023-10-01 13:24:50 +0100

    Begin SamlandLII

---
+++ samlandlii/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SamlandLIIConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "samlandlii"
---
+++ samlandlii/settings.py
@@ -0,0 +1,19 @@
+from django.utils.translation import gettext_lazy as _
+
+from liiweb.settings import *  # noqa
+
+INSTALLED_APPS = ["samlandlii.apps.SamlandLIIConfig"] + INSTALLED_APPS  # noqa
+
+
+JAZZMIN_SETTINGS["site_title"] = "SamlandLII"  # noqa
+JAZZMIN_SETTINGS["site_header"] = "SamlandLII"  # noqa
+JAZZMIN_SETTINGS["site_brand"] = "samlandlii.minersonline.uk"  # noqa
+
+COURT_CODE_MAPPINGS = {
+    "supreme-court": "ZLSC",
+    "court-of-appeal": "ZLCA",
+    "high-court": "ZLHC",
+}
+LANGUAGES = [
+    ("en", _("English")),
+]
